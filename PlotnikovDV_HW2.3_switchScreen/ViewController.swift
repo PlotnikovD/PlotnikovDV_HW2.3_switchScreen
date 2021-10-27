04c4c39027c0d2ce294f3f1c212faed986fe875d
D.Plotnikov <plotnikdima5296@gmail.com>
2021-10-27 13:30:14 +0500

    start screen without segue

--- PlotnikovDV_HW2.3_switchScreen/ViewController.swift
+++ PlotnikovDV_HW2.3_switchScreen/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var alertMessageName = "Your name is User"  //user изменить на ссылку
+    var alertMessagePassword = "Your password is Password" //password изменить на ссылку
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    @IBAction func showAlertButtonTapped(message: String) {
 
+            let alert = UIAlertController(title: "Oops!", message: message, preferredStyle: UIAlertController.Style.alert)
+
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+
+            self.present(alert, animated: true, completion: nil)
+        }
+    
+    
+    @IBAction func forgotName() {
+        showAlertButtonTapped(message: alertMessageName)
+    }
+    
+    @IBAction func forgotPassword() {
+        showAlertButtonTapped(message: alertMessagePassword)
+    }
+    
+    
 }
 
